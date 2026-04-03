d81ddeb8dd65ee49ebb99da9ee5149105eefc9cd
カエデ <yugengaisyabanjun@gmail.com>
2026-04-04 05:17:42 +0900

    day1: start python study

---
+++ day01_hello.py
@@ -0,0 +1,12 @@
+print("Hello, Python!")
+print("2026-04-03 学習開始")
+
+name = "Jun"
+age = 27
+price = 1000
+tax_included = price * 1.1
+
+print(name)
+print(age)
+print(age + 1)
+print(tax_included)
