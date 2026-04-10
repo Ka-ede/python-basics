10e5d4a5776e016330af673e7e0b6942918d19fe
カエデ <yugengaisyabanjun@gmail.com>
2026-04-11 06:23:13 +0900

    day4: learn dict while mini task

---
+++ day04_dict_while_mini_task.py
@@ -0,0 +1,39 @@
+user = {"name": "Jun", "age": 27}
+
+print(user["name"])
+print(user["age"])
+
+user["job"] = "engineer"
+print(user)
+
+product = {"name": "MacBook", "price": 150000}
+
+print(product["name"])
+print(product["price"])
+
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
+
+number = 1
+
+while number <= 5:
+    print("数字:", number)
+    number += 1
+
+item = {"name": "iPhone", "price": 120000}
+
+print(item["name"])
+print(item["price"])
+
+n = 1
+
+while n <= 10:
+    print(n)
+    n += 1
+
+price = item["price"]
+tax_included = price * 1.1
+print(tax_included)
