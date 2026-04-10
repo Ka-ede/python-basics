9651e8af95a58b4f05eb54eedd8c3ece34f908fa
カエデ <yugengaisyabanjun@gmail.com>
2026-04-10 15:41:04 +0900

    day2: learn if for list

---
+++ day02_if_for_list.py
@@ -0,0 +1,28 @@
+age = 17
+
+if age >= 20:
+    print("成人です")
+else:
+    print("未成年です")
+
+score = 85
+
+if score >= 80:
+    print("合格ラインです")
+else:
+    print("もう少しです")
+
+for i in range(5):
+    print(i)
+
+for i in range(1, 6):
+    print("回数:", i)
+
+fruits = ["apple", "banana", "orange"]
+
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+
+for fruit in fruits:
+    print("果物:", fruit)
